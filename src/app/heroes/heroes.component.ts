b03a3342a6cc239b9f58df2cb79c6ccc88334951
Nam Pham <nam.phho@gmail.com>
2018-11-12 00:53:03 +0700

    [pharse] display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from "../Hero";
-
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes-nam',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
@@ -13,11 +13,19 @@ export class HeroesComponent implements OnInit {
   hero : Hero = {
     id : 1,
     name : 'Terminator'
-  }
+  };
+
+  persons = HEROES;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  selectedHero: Hero
+
+  onSelect(a: Hero) {
+    console.log(a);
+    this.selectedHero = a
+  }
 }
